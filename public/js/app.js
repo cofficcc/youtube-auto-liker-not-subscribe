20e07892e129413cf37b50be568385f7fe8e2489
Aleksandr <aleksandr.shliakhturov@gmail.com>
2023-09-21 22:43:04 +0400

    edit html

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+function split(str) {
+    str = str.split('\n');
+    return str;
+}
+
+function start() {
+    var links = split($('#links').val());
+
+    var RegExp = /^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?/;
+
+    links.forEach((element) => {
+        if(RegExp.test(element)){
+            // _blank(element);
+            // console.log(element);
+            var a = document.createElement("a");
+            a.href = element;
+            a.target="_blank";
+            a.click();
+        }
+
+    });
+}
+
+function _blank(link) {
+    window.open(link, '_blank');
+}
